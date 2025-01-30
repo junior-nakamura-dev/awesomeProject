8baacaeba2bfb523f12c62d5601e7b0f1217e5d3
Junior Nakamura <junior_nakamura@icloud.com>
2025-01-30 18:40:28 -0300

    33 - HTTP Post

---
+++ 33/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	c := http.Client{}
+
+	jsonVar := bytes.NewBuffer([]byte(`{"name": "Junior""}`))
+
+	res, err := c.Post("https://google.com", "application/json", jsonVar)
+
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	io.CopyBuffer(os.Stdout, res.Body, nil)
+}
